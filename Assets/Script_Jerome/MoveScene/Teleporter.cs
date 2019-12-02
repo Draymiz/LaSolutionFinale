8a0f6f5842e6f48a112f30fe5a7a9818d59b587c
Draymiz <blaise.jerome84000@gmail.com>
2019-12-02 19:51:19 +0100

    02/12/22019 Squellete Niveau

---
+++ Assets/Script_Jerome/MoveScene/Teleporter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+  //  public GameObject PointB;
+    public Vector3 vector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            transform.position = new Vector3(1,1,1);
+            Debug.Log("Hello");
+        }
+    }
+
+ /*   void OnTriggerEnter(Collision other)
+    {
+        // Si le joueur entre dans le collider ça le tp 
+        transform.position = transform.PointB;
+    }*/
+
+    
+}
