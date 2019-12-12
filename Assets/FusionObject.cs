3fb0fed26e16cb3224ed6ff8c8a3efa4b8d6dd6e
Théo <Nesquique84@gmail.com>
2019-12-12 17:13:56 +0100

    Add mechanique

---
+++ Assets/FusionObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FusionObject : MonoBehaviour
+{
+    public List<GameObject> GameobjectToDestroy;
+    public GameObject GameObjectToActivate;
+
+    public void LaunchFusion()
+    {
+        Debug.Log("FUUUUUUUUUUSSSSION de " + name);
+
+        foreach(GameObject go in GameobjectToDestroy)
+        {
+            Destroy(go);
+        }
+        GameObjectToActivate.SetActive(true);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/FusionPlatform.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FusionPlatform : MonoBehaviour
+{
+    public Camera camera;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player")) {
+            FusionObject fusionObject = FusionRaycast(other.transform);
+            fusionObject?.LaunchFusion();
+        }
+    }
+
+    private FusionObject FusionRaycast(Transform playerTransform)
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit)) {
+            Transform objectHit = hit.transform;
+
+            Debug.DrawRay(playerTransform.position, playerTransform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
+            Debug.Log("Did Hit");
+            FusionObject fusionObject = hit.transform.GetComponent<FusionObject>();
+            if (fusionObject != null)
+                return fusionObject;
+        }
+        else
+        {
+            Debug.DrawRay(playerTransform.position, playerTransform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+        return null;
+    }
+
+}
+
