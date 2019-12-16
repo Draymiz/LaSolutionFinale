5b214a8f68c3dbdb8da9a28403200e09772fdf26
Jerome <blaise.jerome84000@gmail.com>
2019-12-16 02:47:51 +0100

    Add 16/12/2019 Blocking Level 1

---
+++ Assets/Script_Jerome/RotationObjet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationObjet : MonoBehaviour
+{
+    private float speed = 50f;
+
+    void Start()
+    {
+        
+    }
+
+    // Faire une list et réunir tous les GameObject utilisant la méthode update. cela consomme moins
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+
+    }
+}
