2ee1785396ec6457ad17a85286c4e8be8426ec43
Jerome <blaise.jerome84000@gmail.com>
2020-11-24 18:50:37 +0100

    Auto stash before rebase of "origin/JeromeSafeZone"

---
+++ ProjetAvecRaycastEnnemi/EnemySampleNet/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+
+    // this sample script demonstrates that the enemy's line of sight can be changed simply by treating it as a child object which is moved when the parent's transform is moved
+    // in this case we flip the enemies on the x-axis every three seconds so they look the other way
+  
+    public void Flip()
+    {
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, 1);
+
+    }
+}
---
+++ ProjetAvecRaycastEnnemi/EnemySampleNet/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour {
+
+    
+    // this is used to flip the enemies on the x-axis every 3 seconds to demonstrate changing their field of view
+    
+    [SerializeField]
+    EnemyBehaviour[] enemies;
+
+
+    
+    float flipDelay = 3.0f;
+    float nextFlip;
+	// Use this for initialization
+	void Start () {
+        nextFlip = Time.time + flipDelay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       if(Time.time >= nextFlip)
+       {
+           FlipEnemies();
+           nextFlip = Time.time + flipDelay;
+       }
+
+	}
+
+    void FlipEnemies()
+    {
+        foreach(EnemyBehaviour enemy in enemies)
+        {
+            enemy.Flip();
+        }
+
+    }
+
+}
